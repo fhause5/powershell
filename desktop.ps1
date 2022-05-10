519c4d475e6ddc1c1bc1d600ee014a94f967aca4
Igor <45092677+fhause5@users.noreply.github.com>
2022-05-10 20:24:44 +0300

    Update desktop.ps1

--- desktop.ps1
+++ desktop.ps1
@@ -4,6 +4,7 @@ Set-ExecutionPolicy unrestricted
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y virtualbox
 choco install -y vagrant
+choco install -y terragrunt
 choco install -y googlechrome
 choco install -y firefox
 choco install -y postman
